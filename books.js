1e44808ec86df4977ecb770a9e990631435498a9
MoniqueJoeVonnie <jovonnie.mb@gmail.com>
2026-02-07 16:38:50 -0500

    save

--- books.js
+++ books.js
@@ -1,34 +1,56 @@
-function renderBooks() {
+function renderBooks(filter) {
   const booksWrapper = document.querySelector(".books");
 
-  const books = getBooks();
-   
-  
-  const booksHTML = books.map((book) => {
-   return `<div class="book">
-     <figure class="book__img--wrapper">
-       <img class="book__img" src="${books.url}" alt="">
-     </figure>
-     <div class="book__title">
-       ${books.title}
-     </div>
-     <div class="book__ratings">
-       <i class="fas fa-star"></i>
-       <i class="fas fa-star"></i>
-       <i class="fas fa-star"></i>
-       <i class="fas fa-star"></i>
-       <i class="fas fa-star-half-alt"></i>
-     </div>
-     <div class="book__price">
-       <span class="book__price--normal">$${book.originalPrice}</span> $${book.salePrice}
-     </div>
-   </div>`;
-  });
+const books = getBooks();
 
-  booksWrapper.innerHTML = booksHTML;
-  console.log(booksHTML)
-  // booksWrapper.innerHTML = ;
+if (filter === 'LOW_TO_HIGH') {
+  books.sort((a, b) => a.salePrice - b.salePrice);
 }
+else if (filter === 'HIGH_TO_LOW') {
+  books.sort((a, b) => b.salePrice - a.salePrice);
+}
+else if (filter === 'RATING') {
+  books.sort((a, b) => b.rating - a.rating);
+}
+
+let ratingHTML = '';
+
+for (let i = 0; i < 4; ++i) {
+  ratingHTML += '<i class="fas fa-star"></i>\n'
+}
+
+console.log(ratingHTML)
+
+    const booksHTML = books
+      .map((book) => {
+        return `<div class="book">
+          <figure class="book__img--wrapper">
+            <img class="book__img" src="${book.url}" alt="">
+          </figure>
+          <div class="book__title">
+            ${book.title}
+          </div>
+          <div class="book__ratings">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+          </div>
+          <div class="book__price">
+            <span class=>$${book.originalPrice.toFixed(2)}</span> 
+          </div>
+        </div>`;
+      })
+      .join("");
+
+    booksWrapper.innerHTML = booksHTML;
+  }
+
+
+function filterBooks(event) {
+    renderBooks(event.target.value);
+  }
 
 setTimeout(() => {
   renderBooks();
